ab932545dce777ffd0d99f20f1ecfe3b644921a5
Xavier Delamotte <xavier.delamotte@shopify.com>
2022-11-04 19:29:47 +0000

    Rename test

--- test/yubikey_generator_test.rb
+++ test/yubikey_generator_test.rb
@@ -2,7 +2,8 @@
 
 require "test_helper"
 
-class TestYubikeyGenerator < Minitest::Test
+class YubikeyGeneratorTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::YubikeyGenerator::VERSION
   end
@@ -10,7 +11,7 @@ class TestYubikeyGenerator < Minitest::Test
   YUBI_COMMAND = %r{^/(cccc([cbdefghijklnrtuv]{40}|[cbsftdhuneikpglv]{40})|jjjj[jxe.uidchtnbpygk]{40})$}
 
   def test_generate
-    100.times do
+    1000.times do
       word = YubikeyGenerator.generate(colorize: false)
       assert YUBI_COMMAND === word
     end
