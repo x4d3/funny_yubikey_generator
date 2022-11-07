50f2df4a1ae17d2a44731b5740dad77d04831cc4
Xavier Delamotte <xavier.delamotte@shopify.com>
2022-11-07 16:55:54 +0000

    Update version

--- lib/funny_yubikey_generator.rb
+++ lib/funny_yubikey_generator.rb
@@ -5,7 +5,7 @@ require "colorize"
 require "set"
 
 class FunnyYubikeyGenerator
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
   COLORS = %i[red green yellow blue magenta cyan]
   private_constant :COLORS
   class << self
